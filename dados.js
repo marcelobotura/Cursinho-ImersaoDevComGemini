948d390b9ad5d8c0fc33f7592d2773d4704c38ec
marcelobotura <mbsfoz@gmail.com>
2024-09-06 18:51:22 -0300

    cursinho

---
+++ dados.js
@@ -0,0 +1,15 @@
+let dados = [
+    {
+        materia: "Funções",
+        disciplina: "Matemática",
+        conteudo: "Lineares, quadráticas, exponenciais, logarítmicas, compostas, inversas, estudo de gráficos e transformações.",
+        link: "pdfexemplo.html"
+    },
+    {
+        materia: "Matemática Financeira",
+        disciplina: "Matemática",
+        conteudo: "Juros simples e compostos, desconto, amortização, taxas equivalentes, cálculo atuarial básico.",
+        link: "pdfexemplo.html"
+
+    }
+];
